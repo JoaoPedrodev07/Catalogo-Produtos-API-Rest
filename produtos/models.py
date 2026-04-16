137570bed33f2a22d6cf1b07bed00b6f5058b650
Joao <joaopedrolima0115@gmail.com>
2026-04-16 15:29:34 -0300

    salvando imagens no diretorio media

--- produtos/models.py
+++ produtos/models.py
@@ -11,7 +11,7 @@ class Produto(models.Model):
      nome = models.CharField(max_length = 100)
      descricao = models.TextField(max_length = 500)   
      preco = models.DecimalField(max_digits = 10, decimal_places = 2)
-     imagem = models.ImageField(upload_to ='produtos', blank = True, null = True)
+     imagem = models.ImageField(upload_to ='', blank = True, null = True)
      data_criacao = models.DateTimeField(auto_now_add = True)
      categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null = True, blank = True)
   

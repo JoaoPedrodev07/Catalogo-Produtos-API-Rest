da255edd3ec9b84c629abade7e909e8e045f09b1
Joao <joaopedrolima0115@gmail.com>
2026-04-16 15:57:33 -0300

    Corrigir o caminho raiz da mídia para Linux

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -120,5 +120,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT =  MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
